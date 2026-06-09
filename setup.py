77257ba01351169e0045e19e2fca1652f722fa6e
Susmita22110265 <susmita.r@iitgn.ac.in>
2026-06-09 20:59:14 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as f :
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Deep learning Project Chicken Disease classification"
+
+AUTHOR_USER_NAME  ="Susmita_R"
+SRC_REPO = "CNNClassifier"
+AUTHOR_EMAIL = "susmitaramesh28@gmail.com"
+
+
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "A small python package for CNN app",
+    Long_description = long_description, 
+    Long_dsecription_content = "text/markdown",
+    url = f"https://github.com{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker": f"https//github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir = {"": "src"},
+    package = setuptools.find_packages(where = "src")
+)
